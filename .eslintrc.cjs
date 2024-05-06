bc60df47d8f88c21345527aa46669135b377da1c
dashagenze <89853583+dashagenze@users.noreply.github.com>
2024-05-06 19:57:37 +0300

    Revert "Настроить линтер"
    This reverts commit 237beb1b37f754e09509461a00b8f3a16f2bfca5.

--- .eslintrc.cjs
+++
@@ -1,6 +0,0 @@
-const deepmerge = require('deepmerge')
-
-const featureSlicedConfig = require('./eslint/featureSlicedConfig.cjs')
-const baseConfig = require('./eslint/baseConfig.cjs')
-
-module.exports = deepmerge(featureSlicedConfig, baseConfig)
